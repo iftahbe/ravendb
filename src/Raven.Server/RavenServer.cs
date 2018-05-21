76715c8de4b174ae23089bd73010dbcf285885a3
Iftah Ben Zaken <iftahbe@gmail.com>
2018-05-21 09:49:48 +0300

    RavenDB-10692 fix PR comment

--- src/Raven.Server/RavenServer.cs
+++ src/Raven.Server/RavenServer.cs
@@ -1313,11 +1313,6 @@ namespace Raven.Server
                 return true;
             }
 
-            if (tcp.Operation == TcpConnectionHeaderMessage.OperationTypes.TestConnection)
-            {
-                return true;
-            }
-
             if (tcp.Operation == TcpConnectionHeaderMessage.OperationTypes.Heartbeats)
             {
                 // check for the term          
